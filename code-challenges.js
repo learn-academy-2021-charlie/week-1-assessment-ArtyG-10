ab3568af5cd7e3e7664b44ad78929f27d1311f39
ArtyG-10 <a.gourent@outlook.com>
2021-07-09 16:20:05 -0700

    first save

--- code-challenges.js
+++ code-challenges.js
@@ -14,6 +14,18 @@ var temp1 = 35
 var temp2 = 350
 var temp3 = 212
 
+const boilingPoint = (temp) => {
+// -Conditional for boiling point
+  if (temp === 212) {return "212 is at boiling point"}
+// -Conditional for above boiling boilingPoint
+  else if (temp > 212) {return `${temp} is above boiling point`}
+// -Conditional for below boiling boilingPoint
+  else if (temp < 212) {return `${temp} is below boiling point`}
+}
+console.log(boilingPoint(temp1))
+console.log(boilingPoint(temp2))
+console.log(boilingPoint(temp3))
+
 
 
 // --------------------2) Create a function that takes in two arrays of numbers and returns a single array with all the values sorted least to greatest.
@@ -21,6 +33,19 @@ var temp3 = 212
 
 var myNumbers1 = [3, 7, 0, 36, -9]
 var myNumbers2 = [8, -7, 22, 9, 13]
+
+const combineArray = (array1,array2) => {
+// -Combine the arrays using .concat
+  let combinedArrays = array1.concat(array2)
+// -Sort the combined arrays using .sort
+//return combinedArrays.sort();
+//console.log(combineArray(myNumbers1,myNumbers2))
+// -Output is only sorted by the first digit
+// -To fix needed to change the sort to include parameters
+  return combinedArrays.sort(function(a,b){return a - b})
+}
+console.log(combineArray(myNumbers1,myNumbers2))
+
 
 
 
@@ -30,6 +55,16 @@ var myNumbers2 = [8, -7, 22, 9, 13]
 var myString1 = "bravo"
 var myString2 = "charlie"
 
+const reverseString = (string) => {
+// -Convert to array to be able to use .reverse
+let array = string.split("")
+// -Utilize the .reverse mutator
+let reversedArray = array.reverse()
+// -Join the arrays into a string with "" to get rid of spaces
+return reversedArray.join("")
+}
+console.log(reverseString(myString1))
+console.log(reverseString(myString2))
 
 
 // --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number.
